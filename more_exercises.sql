5f1fabf366bbf11c1fc654b972e0f372cca58975
Johnathon Smith <smith.johnathon01@gmail.com>
2021-07-07 12:10:32 -0500

    Answered two more questions in more_exercises.sql. Work in progress

--- more_exercises.sql
+++ more_exercises.sql
@@ -27,3 +27,69 @@ JOIN current_salary_avg ON current_salary_avg.dept_name = current_manager_salari
 #The managers in the production and customer service departments get paid less than their department's average salary.
 
 
+#Use the world database for the questions below.
+# Q1 - What languages are spoken in Santa Monica?
+USE world;
+DESCRIBE city;
+DESCRIBE country;
+DESCRIBE countrylanguage;
+
+SELECT Language, Percentage FROM countrylanguage
+JOIN city ON city.CountryCode = countrylanguage.CountryCode
+WHERE Name = 'Santa Monica'
+ORDER BY Percentage ASC;
+/*
+Japanese	0.2
+Portuguese	0.2
+Vietnamese	0.2
+Korean	0.3
+Polish	0.3
+Tagalog	0.4
+Chinese	0.6
+Italian	0.6
+French	0.7
+German	0.7
+Spanish	7.5
+English	86.2
+*/
+
+# Q2 - How many different countries are in each region?
+SELECT Region, COUNT(*) AS num_countries
+FROM country
+GROUP BY Region
+ORDER BY num_countries ASC;
+/*
+Micronesia/Caribbean	1
+British Islands	2
+Baltic Countries	3
+Melanesia	5
+Antarctica	5
+Australia and New Zealand	5
+North America	5
+Southern Africa	5
+Micronesia	7
+Northern Africa	7
+Nordic Countries	7
+Eastern Asia	8
+Central America	8
+Central Africa	9
+Western Europe	9
+Eastern Europe	10
+Polynesia	10
+Southeast Asia	11
+South America	14
+Southern and Central Asia	14
+Southern Europe	15
+Western Africa	17
+Middle East	18
+Eastern Africa	20
+Caribbean	24
+*/
+
+# Q3 - What is the population for each region?
+
+
+
+
+
+
